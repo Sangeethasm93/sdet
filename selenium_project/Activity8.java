105c3d0020c5d3e21e225dc64988163c40ee1cad
Sangeethasm93 <78068710+Sangeethasm93@users.noreply.github.com>
2021-02-22 11:16:59 +0530

    Changes in Activity 8

--- selenium_project/Activity8.java
+++ selenium_project/Activity8.java
@@ -28,16 +28,18 @@ public class Activity8 {
 		wait = new WebDriverWait(driver, 10);
 		driver.get(
 				"https://alchemy.hguy.co/jobs/wp-login.php?redirect_to=https%3A%2F%2Falchemy.hguy.co%2Fjobs%2Fwp-admin%2F&reauth=1");
-		/*prop = new Properties();
-		ip = new FileInputStream("//NewProject//src//Seleniumproject//Staticvalues.properties");
-		prop.load(ip);*/
+		/*
+		 * prop = new Properties(); ip = new
+		 * FileInputStream("//NewProject//src//Seleniumproject//Staticvalues.properties"
+		 * ); prop.load(ip);
+		 */
 
 	}
 
 	@Test(description = "Actvity 8 - ​Visit the site’s backend and login. ")
 	@Parameters({ "username", "password" })
 	public void backendlogin(String username, String password) throws InterruptedException {
-		
+
 		WebElement Username = driver.findElement(By.id("user_login"));
 		WebElement Password = driver.findElement(By.id("user_pass"));
 		Username.sendKeys(username);
@@ -48,26 +50,24 @@ public class Activity8 {
 		WebElement loggedin = driver.findElement(By.xpath("//div[contains(text(),'Dashboard')]"));
 		Assert.assertTrue(loggedin.isDisplayed());
 	}
-	
-	/*@Test(description = "Actvity 8 - ​Visit the site’s backend and login. ")
-	public void backendlogin2() {
 
-		String Uname = prop.getProperty("username");
-		String Pwd = prop.getProperty("password");
-		WebElement Username = driver.findElement(By.id("user_login"));
-		WebElement Password = driver.findElement(By.id("user_pass"));
-		Username.sendKeys(Uname);
-		Password.sendKeys(Pwd);
-		WebElement loginbutton = driver.findElement(By.xpath("//input[@value='Log In']"));
-		loginbutton.click();
-		Thread.sleep(5000);
-		WebElement loggedin = driver.findElement(By.xpath("//div[contains(text(),'Dashboard')]"));
-		Assert.assertTrue(loggedin.isDisplayed());
+	/*
+	 * @Test(description = "Actvity 8 - ​Visit the site’s backend and login. ")
+	 * public void backendlogin2() {
+	 * 
+	 * String Uname = prop.getProperty("username"); String Pwd =
+	 * prop.getProperty("password"); WebElement Username =
+	 * driver.findElement(By.id("user_login")); WebElement Password =
+	 * driver.findElement(By.id("user_pass")); Username.sendKeys(Uname);
+	 * Password.sendKeys(Pwd); WebElement loginbutton =
+	 * driver.findElement(By.xpath("//input[@value='Log In']"));
+	 * loginbutton.click(); Thread.sleep(5000); WebElement loggedin =
+	 * driver.findElement(By.xpath("//div[contains(text(),'Dashboard')]"));
+	 * Assert.assertTrue(loggedin.isDisplayed());
+	 * 
+	 * }
+	 */
 
-	}*/
-
-	
-	
 	@AfterMethod
 
 	public void TearDown() {
