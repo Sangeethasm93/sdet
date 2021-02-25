b6526e61cea619a50392c9e010fff9a3b39efbc0
Sangeethasm93 <78068710+Sangeethasm93@users.noreply.github.com>
2021-02-25 20:23:03 +0530

    Update Activity1b.groovy

--- API_Project/Activity1b.groovy
+++ API_Project/Activity1b.groovy
@@ -1,45 +1,49 @@
-package Activity_1b
+package GroovyProject1
 
-class Activity_1b 
+class Activity1b 
 {
-	static void main  (def arg)
+public static void main(def args)
 	{
-		File filer = new File("src/API_Project_Activity_1a/ToBeBombarded.txt")
-		filer.createNewFile()
+		File file = new File("src/resources/input.txt")
+		file.createNewFile();
 		
-		filer.write("A quick brown Fox jumped over the lazy Cow\n")
-		filer.append("John Jimbo jingeled happily ever after\n")
-		filer.append("Th1\$ 1\$ v3ry c0nfus1ng\n")
-		filer.append("This is a line with 2 numbers like 12")
+		//Write text to input.txt
+		def inputText = "A quick brown Fox jumped over the lazy Cow\nJohn Jimbo jingeled happily ever after\nThis 1 v3ry c0nfus1ng 33";
+		file.write(inputText);
 		
-		filer.eachLine 
-		{ 
-			
-			if(it =~ /Cow$/)
-			{
-				println "Here inside cow"
-			}
-			
-			if(it =~ /^J/)
-			{
-				println it
-			}
-			
-			if(it =~ /\d{2}/)
-			{
-				println it
-			}
 		
+		println "Match Operations"
+		//print the line that has Cow at the end - Cow$
+		file.eachLine { line ->
+			if(line ==~ /^.*Cow$/) {
+				println "Line that ends with 'Cow' is  $line "
+			}
 		}
-		def matcher1 = filer.text =~ /\S+er/		
-		println matcher1.findAll()
 		
-		def matcher2 = filer.text =~ /\S*\d\W/
-		println matcher2.findAll()
+		//Print the line that start with capital letter 'J' - ^J
+		file.eachLine { line ->
+			if(line ==~ /^J.*/) {
+				println "Line that starts with 'J' is  $line "				
+			}
+		}
+
+		//Print the line that has two numbers one after the other - \d\d
+		file.eachLine { line ->
+			if(line ==~ /.*\d\d.*/) {
+				println "Line that that has 2 numbers one after another is  $line "
+			}
+		}
+		
+		//Print the string(s) that match the pattern '\S+ er' - 
+		println "\n\nFind Operations:"
+		def match1 = file.getText() =~ /\S+er/
+		println "the string(s) that match the pattern '/\\S+er/' are:  ${match1.findAll()}"
+
+		//Print the string(s) that match the pattern '\S*\d\W'
+		def match2 = file.getText() =~ /\S*\d\W/
+		println "the string(s) that match the pattern '/\\S*\\d\\W/' are:  ${match2.findAll()}"
+
 		
 		
-		
-		
-			
 	}
 }
