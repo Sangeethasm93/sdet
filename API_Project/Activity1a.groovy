43179b48ba161d30013f7a32ad05f14ffec43dde
Sangeethasm93 <78068710+Sangeethasm93@users.noreply.github.com>
2021-02-25 20:21:44 +0530

    Update Activity1a.groovy

--- API_Project/Activity1a.groovy
+++ API_Project/Activity1a.groovy
@@ -1,18 +1,16 @@
-package Activity1a
+package GroovyProject1
 
 class Activity1a 
 {
-	static void main(def args)
+	public static void main(def args)
 	{
-		def inputList = [11, 2, 19, 5, "Mango", "Apple", "Watermelon"]
-		println inputList
+		def inputList = [11,2,19,5,"Mango","Apple","Watermelon"]
 		
-		def integerList = inputList.minus(["Mango", "Apple", "Watermelon"])
-		println integerList
+		def strList = inputList.minus([11, 2, 19, 5 ])
+		def intList = inputList.minus(["Mango","Apple","Watermelon"])
 		
-		def stringList = inputList.minus([11, 2, 19, 5])
-		println stringList
-		
+		println "Sorted string list : " + strList.sort()
+		println "Sorted Integer list : " + intList.sort()
 		
 	}
 }
