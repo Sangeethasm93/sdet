b332ba58fc35d13a563947c62db0e725e4e87092
Sangeethasm93 <78068710+Sangeethasm93@users.noreply.github.com>
2021-02-26 11:00:52 +0530

    Update Activity1b.groovy

--- API_Project/Activity1b.groovy
+++ API_Project/Activity1b.groovy
@@ -8,42 +8,27 @@ public static void main(def args)
 		file.createNewFile();
 		
 		//Write text to input.txt
-		def inputText = "A quick brown Fox jumped over the lazy Cow\nJohn Jimbo jingeled happily ever after\nThis 1 v3ry c0nfus1ng 33";
-		file.write(inputText);
+		file.write("A quick brown Fox jumped over the lazy Cow\n")
+		file.append("John Jimbo jingeled happily ever after\n")
+		file.append("Th1\$ 1\$ v3ry c0nfus1ng")
 		
-		
-		println "Match Operations"
-		//print the line that has Cow at the end - Cow$
-		file.eachLine { line ->
-			if(line ==~ /^.*Cow$/) {
-				println "Line that ends with 'Cow' is  $line "
+		file.eachLine { 
+			if(it=~ /Cow$/)	{
+				println it
+			}		
+			if(it=~ /^J/) {
+				println it
 			}
-		}
-		
-		//Print the line that start with capital letter 'J' - ^J
-		file.eachLine { line ->
-			if(line ==~ /^J.*/) {
-				println "Line that starts with 'J' is  $line "				
+			if(it=~ /\d{2}/) {
+				println it
 			}
+			
+			
 		}
-
-		//Print the line that has two numbers one after the other - \d\d
-		file.eachLine { line ->
-			if(line ==~ /.*\d\d.*/) {
-				println "Line that that has 2 numbers one after another is  $line "
-			}
-		}
-		
-		//Print the string(s) that match the pattern '\S+ er' - 
-		println "\n\nFind Operations:"
-		def match1 = file.getText() =~ /\S+er/
-		println "the string(s) that match the pattern '/\\S+er/' are:  ${match1.findAll()}"
-
-		//Print the string(s) that match the pattern '\S*\d\W'
-		def match2 = file.getText() =~ /\S*\d\W/
-		println "the string(s) that match the pattern '/\\S*\\d\\W/' are:  ${match2.findAll()}"
-
-		
-		
+		def match1= (file.text=~/\S+er/).findAll()
+				println match1
+		def match2= file.text=~/\S*\d\W/
+		        println match2.findAll()
+				println match2
 	}
 }
